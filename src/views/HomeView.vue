812b22a9937d03693ff4f5fe9afeab13ea3cfaa0
Rafael Bañón <rafael.banon-martin@capgemini.com>
2023-01-13 10:20:34 +0100

    Instalación previa

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+  </div>
+  <h1>Hola mundo</h1>
+  <button class="btn btn-primary">Primary</button>
+  <button class="btn btn-secondary">Secondary</button>
+  <button class="btn btn-success">Success</button>
+</template>
+
