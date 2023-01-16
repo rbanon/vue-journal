cd4e5ec81e4a1696128180ab252b7afec225e918
Rafael Bañón <rafael.banon-martin@capgemini.com>
2023-01-16 16:43:50 +0100

    Daybook component terminado

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -2,19 +2,19 @@ export default () => ({
     isLoading: true,
     entries: [
         {
-            id: new Date().getTime(),
+            id: '1',
             date: new Date().toDateString(),
             text: 'Commodo ex eu cillum incididunt cupidatat pariatur nulla ad incididunt occaecat nostrud. Consequat nulla veniam nostrud laborum. Cupidatat ad voluptate sint et ipsum aute ipsum magna nulla ea officia ex excepteur. Laborum sunt proident exercitation consectetur consequat ullamco occaecat culpa aliquip nulla reprehenderit.',
             picture: null
         },
         {
-            id: new Date().getTime()+1000,
+            id: '2',
             date: new Date().toDateString(),
             text: 'Adipisicing nulla esse duis deserunt adipisicing sit ea proident adipisicing. Sit officia nostrud est consequat est occaecat amet minim excepteur commodo velit enim. Amet aute reprehenderit esse anim mollit aute dolore tempor aliquip. Ea velit consequat laborum duis sint velit esse eu. Ea eiusmod nostrud mollit id aliquip dolor magna adipisicing sunt proident. Laboris proident officia occaecat duis nostrud minim ullamco nostrud eiusmod quis. Amet enim ut laboris consectetur sit sint reprehenderit fugiat nisi.',
             picture: null
         },
         {
-            id: new Date().getTime()+2000,
+            id: '3',
             date: new Date().toDateString(),
             text: "Aute aliquip officia id laboris cillum adipisicing. Laboris incididunt irure dolor reprehenderit ad deserunt labore enim cillum incididunt aliqua cupidatat aute sint. Magna nostrud velit labore reprehenderit consequat. Esse ullamco incididunt amet labore tempor ad incididunt id anim est amet. Duis irure labore enim consectetur. Irure consequat qui sit sit id tempor consectetur commodo tempor amet minim. Et veniam aliqua eiusmod in ut mollit reprehenderit occaecat officia fugiat minim sunt.",
             picture: null
--- src/modules/daybook/views/EntryView.vue
+++ src/modules/daybook/views/EntryView.vue
@@ -1,43 +1,108 @@
 <template>
-  <div class="entry-title d-flex justifuy-content-between p2">
-    <div>
-        <span class="text-success fs-3 fw-bold">12</span>
-        <span class="max-1 fs-3">Enero</span>
-        <span class="mx-2 fs-4 fw-light">2023, jueves</span>
-    </div>
 
-    <div>
-        <button class="btn btn-danger mx-2">
-            Borrar <i class="fa fa-trash-alt"></i>
-        </button>
-        <button class="btn btn-primary">
-            Subir foto <i class="fa fa-upload"></i>
-        </button>
-    </div>
-  </div>
-  <hr>
-  <div class="d-flex flex-column px-3 h-75">
-    <textarea
-        placeholder="Qué sucedió hoy?">
-    </textarea>
-  </div>
+    <template v-if="entry">
+        <div class="entry-title d-flex justify-content-between p-2">
+            
+            <div>
+                <span class="text-success fs-3 fw-bold">{{ day }}</span>
+                <span class="mx-1 fs-3">{{ month }}</span>
+                <span class="mx-2 fs-4 fw-light">{{ yearDay }}</span>
+            </div>
 
-  <Fab
-    icon="fa-save"
-  />
-  <img
-    src="https://concepto.de/wp-content/uploads/2015/03/paisaje-e1549600034372.jpg"
-    alt="entry-picture"
-    class="img-thunmbnail">
+            <div>
+                <button class="btn btn-danger mx-2">
+                    Borrar
+                    <i class="fa fa-trash-alt"></i>
+                </button>
+
+                <button class="btn btn-primary">
+                    Subir foto
+                    <i class="fa fa-upload"></i>
+                </button>
+            </div>
+        </div>
+
+        <hr>
+        <div class="d-flex flex-column px-3 h-75">
+            <textarea
+                v-model="entry.text"
+                placeholder="¿Qué sucedió hoy?"
+            ></textarea>
+        </div>
+
+
+        <img 
+            src="https://www.robertlandscapes.com/wp-content/uploads/2014/11/landscape-322100_1280.jpg" 
+            alt="entry-picture"
+            class="img-thumbnail">
+
+    </template>
+
+    <Fab 
+        icon="fa-save"
+    />
+
 </template>
-
 <script>
 import { defineAsyncComponent } from 'vue'
 
+import { mapGetters } from "vuex";
+import getDayMonthYear from "../helpers/getDayMonthYear";
+
 export default {
+    props: {
+        id: {
+            type: String,
+            required: true
+        }
+    },
+
     components: {
         Fab: defineAsyncComponent(() => import('../components/Fab.vue'))
+    },
+
+    data(){
+        return{
+            entry: null
+        }
+    },
+
+    computed: {
+        ...mapGetters('journal', ['getEntryById']),
+        day() {
+            const { day } = getDayMonthYear( this.entry.date )
+            return day
+        },
+        month() {
+            const { month } = getDayMonthYear( this.entry.date )
+            return month
+        },
+        yearDay() {
+            const { yearDay } = getDayMonthYear( this.entry.date )
+            return yearDay
+        }
+    },
+
+    methods: {
+        loadEntry(){
+            const entry = this.getEntryById( this.id )
+            if ( !entry ) return this.$router.push({ name: 'no-entry' })
+
+            this.entry = entry
+        }
+    },
+    
+    created() {
+        this.loadEntry()
+    },
+
+    watch: {
+        id() {
+            this.loadEntry()
+        }
     }
+
+    
 }
 </script>
 
