837bf5a3de666f710cd1a32061018ff2e08e46b9
Rafael Bañón <rafael.banon-martin@capgemini.com>
2023-01-16 13:38:38 +0100

    GetEntriesByTerm

--- src/modules/daybook/store/journal/getters.js
+++ src/modules/daybook/store/journal/getters.js
@@ -2,10 +2,13 @@
 //  return state
 // }
 
-export const getentriesByTern = ( /*state*/ ) =>  {
- 
+export const getEntriesByTerm = ( state ) => ( term = '' ) => {
+
+    if ( term.length === 0 ) return state.entries
+
+    return state.entries.filter( entry => entry.text.toLowerCase().includes( term.toLocaleLowerCase() ) )
 }
 
-export const getentryById = ( /*state*/ ) =>  {
+export const getEntryById = ( /*state*/ ) =>  {
  
 }
